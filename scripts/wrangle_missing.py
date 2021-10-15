502fe186e8dde7ac4597c932eb8a32cba7669541
jakob-schuster <64141819+jakob-schuster@users.noreply.github.com>
2021-10-15 14:20:06 +1100

    added median income checking to the missingdata file

--- scripts/wrangle_missing.py
+++ scripts/wrangle_missing.py
@@ -2,6 +2,9 @@
     code to handle all the schools with missing entries
 """
 
+import csv
+import statistics
+
 SCHOOL_FINAL_CSV = 'wrangled_df/school_final.csv'
 MISSING_FINAL_CSV = 'wrangled_df/missing_school_final.csv'
 
@@ -16,7 +19,7 @@ def wrangle_missing(school_final_csv=SCHOOL_FINAL_CSV, missing_final_csv=MISSING
             missing_final.writelines(lines[0])
             missing_final.writelines([line for line in lines if line[-3:-1] == ',,'])
 
-def get_sector_counts_dict(missing_final_csv=MISSING_FINAL_CSV):
+def get_sector_counts_dict(filename=MISSING_FINAL_CSV):
     """
     counts up the sectors that each school is in,
     returns a dictionary of these
@@ -24,23 +27,38 @@ def get_sector_counts_dict(missing_final_csv=MISSING_FINAL_CSV):
 
     sector_counts = {}
 
-    with open(missing_final_csv, 'r') as missing_final:
-        lines = missing_final.readlines()
-        entries = lines[0].split(',')
-
-        sector_index = entries.index('Education Sector')
-
-        for line in lines[1:]:
-            entries = line.split(',')
-
-            if entries[sector_index] not in sector_counts:
-                sector_counts[entries[sector_index]] = 0
-            sector_counts[entries[sector_index]] += 1
-        
+    with open(filename, 'r') as missing_final:
+        missing_csv = csv.DictReader(missing_final)
+        for row in missing_csv:
+            if row['Education Sector'] not in sector_counts:
+                sector_counts[row['Education Sector']] = 0
+            sector_counts[row['Education Sector']] += 1
+    
     return sector_counts
+
+def get_median_income(filename=MISSING_FINAL_CSV):
+    """
+    gets the median income among schools
+    """
+
+    incomes = []
+    with open(filename, 'r') as missing_final:
+        missing_csv = csv.DictReader(missing_final)
+        
+        for row in missing_csv:
+            if row['Median Income in Postcode'] != '':
+                incomes.append(float(row['Median Income in Postcode']))
+
+    return statistics.median(incomes)
 
 if __name__=='__main__':
     wrangle_missing()
     sector_counts = get_sector_counts_dict()
+    sector_counts_orig = get_sector_counts_dict(filename=SCHOOL_FINAL_CSV)
+    median = get_median_income()
+    median_orig = get_median_income(filename=SCHOOL_FINAL_CSV)
 
     print(sector_counts)
+    print(median)
+    print(sector_counts_orig)
+    print(median_orig)
